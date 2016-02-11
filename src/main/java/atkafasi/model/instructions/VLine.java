de66959f52de3874903b8c0489a9e8cf09801574
canmogol <canmogol@gmail.com>
2016-02-11 10:40:07 +0200

    x and y re-placed

--- src/main/java/atkafasi/model/instructions/VLine.java
+++ src/main/java/atkafasi/model/instructions/VLine.java
@@ -3,11 +3,11 @@ package atkafasi.model.instructions;
 public class VLine extends Line {
 
 	
-	public VLine(int x1, int x2, int y) {
-		super(x1, y, x2, y);
-		if(x1>x2){
-			setC1(x2);
-			setC2(x1);
+	public VLine(int y1, int y2, int x) {
+		super(y1, x, y2, x);
+		if(y1>y2){
+			setC1(y2);
+			setC2(y1);
 		}
 	}
 
